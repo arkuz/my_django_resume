cce25e833862a2fc232099652ff6891b9053fa08
Artem Kuzmenko <arkuzka@mail.ru>
2019-12-13 20:49:55 +0300

    updated views.py

--- my_resume/resume/views.py
+++ my_resume/resume/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Resume
 
 
 def index(request):
-    return render(request, 'resume/index.html')
+    resume = None
+    try:
+        resume = Resume.objects.get(is_active=True)
+    except Resume.DoesNotExist:
+        pass
+    print(resume)
+    return render(request, 'resume/index.html', context={'resume': resume})
+
