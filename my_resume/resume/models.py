d13e174c91aec3ac344eef554f0cc70cf75c4779
Artem Kuzmenko <arkuzka@mail.ru>
2019-12-13 20:49:38 +0300

    updateв models.py

--- my_resume/resume/models.py
+++ my_resume/resume/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Resume(models.Model):
+    is_active = models.BooleanField(verbose_name='Активна?')
+    firstname = models.CharField(max_length=50, verbose_name='Имя')
+    lastname = models.CharField(max_length=50, verbose_name='Фамилия')
+    patronomic = models.CharField(max_length=50, blank=True, verbose_name='Отчество')
+    birthday = models.DateField(verbose_name='Дата рождения')
+    photo = models.FileField(blank=True, verbose_name='Фотография')
+
+    def __repr__(self):
+        return f'Resume: {self.lastname} {self.firstname}'
+
+    def __str__(self):
+        return f'{self.lastname} {self.firstname}'
+
+    class Meta:
+        verbose_name_plural = 'Резюме'
+        verbose_name = 'Резюме'
