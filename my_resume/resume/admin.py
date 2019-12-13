b5bcd27e8d1a555cf126f73d77548d23c5b9a396
Artem Kuzmenko <arkuzka@mail.ru>
2019-12-13 20:48:57 +0300

    updated admin.py

--- my_resume/resume/admin.py
+++ my_resume/resume/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Resume
+
+class ResumeAdmin(admin.ModelAdmin):
+
+    list_display = ['lastname', 'firstname', 'patronomic', 'birthday', 'is_active']
+    list_display_links = ['lastname', 'firstname', 'patronomic']
+    search_fields = ['lastname', 'firstname', 'birthday']
+
+admin.site.register(Resume, ResumeAdmin)
+
